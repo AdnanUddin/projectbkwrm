19322510477ea929685e66498ed5005757a93dad
Adnan Uddin <adnan.uddin6@gmail.com>
2017-07-31 18:49:44 -0400

    removed Jade Engine

--- app.js
+++ app.js
@@ -11,8 +11,10 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+// app.engine('html', require('ejs').renderFile);
+// app.use(express.static(path.join(__dirname,'public')));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -43,16 +45,15 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.use(express.static('public'));
 
-app.get('/', function(req,res){
-    res.sendFile(__dirname + '/' + 'index.html');
-});
+// app.get('/', function(req,res){
+//     res.sendFile(__dirname + '/' + 'index.html');
+// });
   
 module.exports = app;
 
-var server = app.listen(8081, "127.0.0.1", function(req,res){
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log("example app listening to http://%s:%s", host, port);
-});
+// var server = app.listen(8081, "127.0.0.1", function(req,res){
+//     var host = server.address().address;
+//     var port = server.address().port;
+//     console.log("example app listening to http://%s:%s", host, port);
+// });
