759389f5cff5ef665848d8ee61f18c627a339b6b
Adnan Uddin <adnan.uddin6@gmail.com>
2017-07-31 17:16:12 -0400

    added index.html

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 var express = require('express');
-
-
+var fs = require('fs');
 var app = express();
 
-console.log('Hello World!');
+
+app.use(express.static('public'));
+
+app.get('/', function(req,res){
+    res.sendFile(__dirname + '/' + 'index.html');
+});
+
+var server = app.listen(8081, "127.0.0.1", function(req,res){
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("example app listening to http://%s:%s", host, port);
+});
